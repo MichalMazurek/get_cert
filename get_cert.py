7d4d75078013109254ba0f91cc3dba9fb7f4dfee
Michal Mazurek <michal@mazurek-inc.co.uk>
2019-07-13 10:41:21 +0100

    adding get_cert script and changing socket creation mechanism

--- get_cert.py
+++ get_cert.py
@@ -1,17 +1,26 @@
 import ssl
 from urllib.parse import urlparse
 import click
+import socket
 
 
 def get_certificate(url: str) -> str:
-    """Download certificate from remote server."""
+    """Download certificate from remote server.
+
+    Args:
+        url (str): url to get certificate from
+
+    Returns:
+        str: certificate string in PEM format
+    """
     parsed_url = urlparse(url)
 
     hostname = parsed_url.hostname
     port = int(parsed_url.port or 443)
-    conn = ssl.create_connection((hostname, port))
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
     sock = context.wrap_socket(conn, server_hostname=hostname)
+    sock.connect((hostname, port))
     return ssl.DER_cert_to_PEM_cert(sock.getpeercert(True))
 
 
